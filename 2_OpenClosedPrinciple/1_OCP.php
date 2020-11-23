58993eb0f14be8dea202d9cf1228758592989421
LAMPEngineer <LAMP.Engineer@gmail.com>
2020-11-23 21:26:38 +0530

    OCP second example

--- 2_OpenClosedPrinciple/1_OCP.php
+++ 2_OpenClosedPrinciple/1_OCP.php
@@ -15,10 +15,59 @@
 /**
  * OCP Violation:
  * 
- * Lets consider example of GasStation the modality to put gas in vehicles 
+ * Lets consider example of GasStation the modality to put gas in vehicles. 
+ * The code works but the problem will apear if we would like to put gas for another type of vehicle, 
+ * for that we should update "putGasInVehicle()" method, that violate OPC 
  * 
  */
 
+class GasStation
+{
+	public function putGasInVehicle(Vehicle $vehicle)
+	{
+		if ($vehicle->getType() == 1) 
+			$this->putGasInCar($vehicle);
+		elseif ($vehicle->getType() == 2) 
+			$this->putGasInMotorcycle($vehicle);		
+
+	}
+
+
+	public function putGasInCar(Car $car)
+	{
+		$car->setTank(50);
+	}
+
+
+	public function putGasInMotorcycle(Motorcycle $motorcycle)
+	{
+		$motorcycle->setTank(20);
+	}
+
+}
+
+
+class Vehicle
+{
+	protected $type;
+	protected $tank;
+
+	public function getType(){return $this->type;}
+	public function setTank($tank){$this->tank = $tank;}
+
+}
+
+
+class Car extends Vehicle
+{
+	protected $type = 1;
+}
+
+
+class Motorcycle extends Vehicle
+{
+	protected $type = 2;
+}
 
 
 
@@ -27,7 +76,35 @@
  * Refactor:
  *
  * 
- * To solve this problem, - (Abstract class)
+ * Refactoring using Abstract class - We could inherit from abstract Vehicle class and override abstrat method putGasIn in each child classes. 
  * 
  */
+
+class GasStation
+{
+	public function putGasInVehicle(Vehicle $vehicle)
+	{
+		$vehicle->putGasIn();
+	}
+}
+
+
+abstract class Vehicle 
+{
+	protected $tank;
+	public function setTank($tank){$this->tank = $tank;}
+	abstract public function putGasIn();
+}
+
+
+class Car extends Vehicle
+{
+	public function putGasIn(){$this->setTank(50);}
+}
+
+
+class Motorcycle extends Vehicle
+{
+	public function putGasIn(){$this->setTank(20);}
+}
 
