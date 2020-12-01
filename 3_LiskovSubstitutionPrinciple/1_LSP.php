0e5137477f10839fed74a64ec65aa5d96735a599
LAMPEngineer <LAMP.Engineer@gmail.com>
2020-12-01 21:32:54 +0530

    link index to files

--- 3_LiskovSubstitutionPrinciple/1_LSP.php
+++ 3_LiskovSubstitutionPrinciple/1_LSP.php
@@ -1,7 +1,76 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 /**
  * Liskov Substitution Principle (LSP)
+ *
+ * This principle declares that - 
+ * Derived classes must be substitutable for their base classes.
+ * 
  * 
  */
 
-echo "Liskov Substitution Principle (LSP)";
+
+
+/**
+ * LSP Violation:
+ *
+ * Let's assume that the Rectangle object is used in the application. We extend the application and add the Square class. The square class is returned by a factory pattern, based on some conditions and we don't know the exect what type of object will be returned.
+ * 
+ */
+
+class Rectangle
+{
+	protected $width;
+	protected $height;
+
+	//setters and getters...
+	public function setWidth($width){$this->width = $width;}
+	public function getWidth(){return $this->width;}
+
+	public function setHeight($height){$this->height = $height;}
+	public function getHeight(){return $this->height;}
+
+
+	public function getArea()
+	{
+      return $this->height * $this->width;
+	}
+
+}
+
+class Square extends Rectangle
+{
+	public function setWidth($width)
+	{
+		$this->width  = $width;
+		$this->height = $width;
+	}
+
+	public function setHeight($height)
+	{
+		$this->height = $height;
+		$this->width  = $height;
+	}
+
+}
+
+
+
+/*$rectangle = new Rectangle();
+$rectangle->setWidth(5);
+$rectangle->setHeight(10);
+
+
+echo "<br/>Width= ".$rectangle->getWidth()."<br/>Heihght = ".$rectangle->getHeight();
+echo "<br/>Area= ". $rectangle->getArea();*/
+
+
+$square = new Square();
+$square->setWidth(5);
+$square->setHeight(10); 
+
+echo "<br/>Width= ".$square->getWidth()."<br/>Heihght = ".$square->getHeight();
+echo "<br/>Area= ". $square->getArea();
--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 	<h1>Testing SOLID PHP</h1>
 	
 	<ul>
-		<li><a href="./1_SRP.php">Single-Responsibility Principle (SRP)</a></li>
-		<li><a href="./2_OCP.php">Open-Closed Principle (OCP)</a></li>
-		<li><a href="./3_LSP.php">Liskov Substitution Principle (LSP)</a></li>
-		<li><a href="./4_ISP.php">Interface Segregation Principle (ISP)</a></li>
-		<li><a href="./5_DIP.php">Dependency Inversion Principle (DIP)</a></li>
+		<li><a href="./1_SingleResponsibilityPrinciple/1_SRP.php">Single-Responsibility Principle (SRP)</a></li>
+		<li><a href="./2_OpenClosedPrinciple/1_OCP.php">Open-Closed Principle (OCP)</a></li>
+		<li><a href="./3_LiskovSubstitutionPrinciple/1_LSP.php">Liskov Substitution Principle (LSP)</a></li>
+		<li><a href="./4_InterfaceSegregationPrinciple/1_ISP.php">Interface Segregation Principle (ISP)</a></li>
+		<li><a href="./5_DependencyInversionPrinciple/1_DIP.php">Dependency Inversion Principle (DIP)</a></li>
 	</ul>
 	
 </body>
