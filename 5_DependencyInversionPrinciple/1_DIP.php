fe762068d755cd17604204c226e90fce70a1d4e7
LAMPEngineer <LAMP.Engineer@gmail.com>
2020-12-05 21:40:39 +0530

    Dependency-Inversion Principle

--- 5_DependencyInversionPrinciple/1_DIP.php
+++ 5_DependencyInversionPrinciple/1_DIP.php
@@ -3,6 +3,85 @@
  * Dependency Inversion Principle (DIP)
  *
  *  This principle declares that - 
+ *  Dependencies within the system are built on the basis of abstractions. 
+ *  The top-level modules do not depend on the lower-level modules. 
+ *  Abstractions should not depend on details. Details must depend on 
+ *  abstractions." 
+ *  
+ *  This definition can be shortened - "the dependencies 
+ *  should be based on abstractions, not details.
  * 
  */
 
+
+/**
+ * ISP Violation:
+ * 
+ */
+class Worker
+{
+	public function work(){/*...*/}
+}
+
+
+class Manager
+{
+	
+	public function manage()
+	{
+		$worker = new Worker();
+		$worker->work();
+	}
+}
+
+
+/**
+ * Issue here is the Manager class depends on the Worker class.
+ * 
+ */
+
+
+
+/**
+ * Refactor: 
+ * 
+ * In order to get rid of the dependence on a particular class, 
+ * we need to make sure that Manager depends on abstraction, 
+ * ie. From the IWorker interface.
+ *
+ * 
+ */
+
+
+interface IWorker
+{
+	public function work();
+}
+
+
+class Worker implements IWorker
+{
+	public function work(){/*...*/}
+}
+
+
+class SuperWorker implements IWorker
+{
+	public function work(){/*...*/}
+}
+
+
+class Manager
+{
+	private $worker;
+
+	public function setWorker(IWorker $worker)
+	{
+		$this->worker = $worker;
+	}
+
+	public function manage()
+	{
+		$this->worker->work();
+	}
+}
